bb934a28c39b56acab92180157a2bdb1669c1a05
onlymisaky <onlymisaky@qq.com>
2022-08-16 10:26:43 +0800

    🐞 fix(Array): flat, flatMap

--- Array/flatMap.js
+++ Array/flatMap.js
@@ -1,20 +1,13 @@
 const isArr = (arr) => arr instanceof Array;
 
-// todo depth = 1;
-
 function flatMap(cb) {
   const result = [];
   for (let index = 0; index < this.length; index++) {
-    let item = this[index];
-    if (isArr(item)) {
-      // todo
-      result.push(...item.flatMap(cb))
-    } else {
-      item = cb.call(this, item, index, Object(this));
-      result.push(item);
-    }
+    let item = cb.call(this, this[index], index, Object(this));
+    result.push(item);
   }
-  return result;
+  // todo
+  return result.flat(1);
 }
 
-Array.prototype.flatMap = flatMap; 
+Array.prototype.flatMap = flatMap;
